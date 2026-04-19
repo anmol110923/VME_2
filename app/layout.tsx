a52db2ddd0589f3fe8716dffa4ca50b5f47e149d
Kunwar Anmol Singh <kunwaranmol1025singh@gmail.com>
2026-04-19 23:00:37 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,10 @@ import Script from 'next/script';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5602912928391127"
+     crossorigin="anonymous"></script>
+      </head>
       
       <body>
         {children}
